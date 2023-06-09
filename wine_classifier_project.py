6515d7f092e156af1edb1c6ac99118debc47b72a
Rafaela Altermann <rafaela@example.com>
2023-06-09 12:01:51 +0200

    code improvements

--- wine_classifier_project.py
+++ wine_classifier_project.py
@@ -30,11 +30,20 @@ class WineClassifier:
         score = self.model.score(self.X_test, self.y_test)
         print("Accuracy:", score)
 
-    def predict(self, data):
+    def predict(self, wine_characteristics):
         if self.model is None:
             print("Error: model has not been trained yet!")
             return
-        return self.model.predict(data)
+        return self.model.predict(wine_characteristics).item()
+
+    def get_wine_characteristics(self, database_entry):
+        return self.X.iloc[database_entry].values.reshape( 1,-1)
+
+    def get_wine_color(self, database_entry):
+        return self.y.iloc[database_entry].item()
+
+    def get_number_of_entries(self):
+        return self.data.shape[0]
 
 
 wine_classifier = WineClassifier("wine_data.csv")
@@ -42,41 +51,24 @@ wine_classifier.train_model()
 wine_classifier.test_model()
 
 
-# Load the data from the CSV file
-wine_data = pd.read_csv("wine_data.csv")
+# WineClassifier uses 0 and 1 for wine colors
+wine_colors = ["red", "white"]
 
-# Get the number of rows and columns
-num_rows = wine_data.shape[0]
-num_columns = wine_data.shape[1]
 
-# Print the number of rows and columns
-print("Number of rows:", num_rows)
-print("Number of columns:", num_columns)
+# Select a random wine from the table and predict its color
+random_entry = random.choice(range(0, wine_classifier.get_number_of_entries()))
+random_wine_characteristics = wine_classifier.get_wine_characteristics(random_entry)
+prediction = wine_classifier.predict(random_wine_characteristics)
 
-# Here's the code to randomly select a wine from the table and determine if it is red or white:
-
-# Select a random wine
-random_wine = random.choice(wine_data['style'])
-
-# Determine if the wine is red or white
-if random_wine == 'red':
-    print("Here's the code to randomly select a wine from the table : This wine is red.")
+# Check if prediction is correct
+if prediction == wine_classifier.get_wine_color(random_entry):
+    print("The prediction was correct! The wine is:", wine_colors[prediction])
 else:
-    print("Here's the code to randomly select a wine from the table :This wine is white.")
+    print("The prediction was not correct :(")
 
-
-
-# Predict wine color at index ex.5320
-predicted_label = wine_classifier.predict(wine_classifier.X.iloc[5320].values.reshape( 1,-1))
-
-if predicted_label == 0:
-    print("The wine at index 5320 is red.")
-else:
-    print("The wine at index 5320 is white.")
 
 
 # Count the occurrences of each wine color
-wine_colors = ["red", "white"]
 counts = wine_classifier.data['style'].value_counts()
 
 # Plotting the bar chart with the highlighted color
@@ -92,9 +84,7 @@ plt.ylabel('Frequency')
 plt.show()
 
 # Bar chart of the 'alcohol' variable
-
 plt.bar(wine_colors, counts)
 plt.xlabel('Wine Color')
 plt.ylabel('alcohol')
 plt.show()
-
